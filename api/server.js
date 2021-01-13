72fe7bea52dfafcc886c4fc44918d501976a0519
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-13 19:51:18 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet')
+const cors = require('cors')
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
