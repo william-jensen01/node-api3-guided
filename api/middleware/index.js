60554327f5aac8a3fc6281afd309e0fcbff9e184
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-13 19:05:48 +0100

    wip

---
+++ api/middleware/index.js
@@ -0,0 +1,23 @@
+async function checkHubId(req, res, next) {
+  console.log('checking hub id')
+  res.set('X-Lambda-Header', 'rocks')
+  try {
+    const hub = await Hubs.findById(req.params.id)
+    if (hub) {
+      req.hub = hub
+      next()
+    } else {
+      res.status(404).json(`hub with id ${req.params.id} not found`)
+    }
+  } catch (error) {
+    res.status(500).json('ouch')
+  }
+  // inside middlewares we have access to req and res objects
+  // here we can query db, modify request, validate request...
+
+  // after we are done we make a choice:
+  //  - allow the request to proceed to the next middleware
+  //  - or send a response to the client
+}
+
+module.exports = checkHubId
