d6d4abb1ce324caa7570b59373a7a8da632d800b
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-13 18:27:35 +0100

    wip

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -5,6 +5,8 @@ const Messages = require('../messages/messages-model.js');
 
 const router = express.Router();
 
+function checkHubId
+
 router.get('/', (req, res) => {
   Hubs.find(req.query)
     .then(hubs => {
