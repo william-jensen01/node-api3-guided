3fcf1fb2a6c57ba5ad294435b95466653ecc8020
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-13 18:56:42 +0100

    wip

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -42,21 +42,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', checkHubId, (req, res) => {
-  Hubs.findById(req.params.id)
-    .then(hub => {
-      if (hub) {
-        res.status(200).json(hub);
-      } else {
-        res.status(404).json({ message: 'Hub not found' });
-      }
-    })
-    .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the hub',
-      });
-    });
+  res.status(200)
 });
 
 router.post('/', (req, res) => {
