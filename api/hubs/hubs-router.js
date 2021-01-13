9ba31a53f823efe56d4d188503a19d22e89e08b3
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-13 19:04:18 +0100

    wip

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -5,27 +5,7 @@ const Messages = require('../messages/messages-model.js');
 
 const router = express.Router();
 
-async function checkHubId(req, res, next) {
-  console.log('checking hub id')
-  res.set('X-Lambda-Header', 'rocks')
-  try {
-    const hub = await Hubs.findById(req.params.id)
-    if (hub) {
-      req.hub = hub
-      next()
-    } else {
-      res.status(404).json(`hub with id ${req.params.id} not found`)
-    }
-  } catch (error) {
-    res.status(500).json('ouch')
-  }
-  // inside middlewares we have access to req and res objects
-  // here we can query db, modify request, validate request...
-
-  // after we are done we make a choice:
-  //  - allow the request to proceed to the next middleware
-  //  - or send a response to the client
-}
+const checkHubId = require('../middleware/middleware.js')
 
 router.get('/', (req, res) => {
   Hubs.find(req.query)
